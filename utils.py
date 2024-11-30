d4469185f7f406cb9735b7961ce022f92985e683
johansle <johansle@kth.se>
2024-11-30 14:35:35 +0000

    Split trunk strategy + comments

--- utils.py
+++ utils.py
@@ -1,21 +1,25 @@
 import torch
-import torch.nn as nn
 import matplotlib.pyplot as plt
 
-# Define network class (we use the same base structure for both branch and trunk)
-class FNN(nn.Module):
+# Fully connected NN (use same structure for branch and trunk)
+class FNN(torch.nn.Module):
     def __init__(self, layer_sizes, activation='relu'):
         super().__init__()
         
+        # Set activation function
         self.activation = {'relu' : torch.relu, 'tanh':torch.tanh}[activation]
 
+        # Create layers
         self.linears = torch.nn.ModuleList()
         for i in range(1, len(layer_sizes)):
             layer = torch.nn.Linear(layer_sizes[i - 1], layer_sizes[i])
+            # Initialize weights (normal distribution)
             torch.nn.init.xavier_normal_(layer.weight)
+            # initialize biases (zeros)
             torch.nn.init.zeros_(layer.bias)
             self.linears.append(layer)
         
+    # FNN forward pass
     def forward(self, inputs):
         x = inputs
         for linear in self.linears[:-1]:
@@ -23,86 +27,117 @@ class FNN(nn.Module):
         x = self.linears[-1](x)
         return x
     
-    
-class DeepONet(nn.Module):
+# DeepONet class 
+class DeepONet(torch.nn.Module):
     def __init__(self, layer_sizes_branch, layer_sizes_trunk, dim_out=1, activation='relu'):
         super(DeepONet, self).__init__()
         
+        # Final output dimension
         self.dim_out = dim_out
                 
-        # Initialize branch and trunk networks
+        # Initialize branch networks (dim_out many)
         self.branches = torch.nn.ModuleList([FNN(layer_sizes_branch, activation) for _ in range(self.dim_out)]) 
-        self.trunks =  torch.nn.ModuleList([FNN(layer_sizes_trunk, activation) for _ in range(self.dim_out)]) 
-        
-        # bias term
-        self.bias = nn.Parameter(torch.zeros(1, 1, self.dim_out))
+        # Initialize one trunk network (using split trunk strategy)
+        self.trunk = FNN(layer_sizes_trunk, activation)
+        # Initialize bias term
+        self.bias = torch.nn.Parameter(torch.zeros(1, 1, self.dim_out))
 
-    
+    # DeepONet forward pass
     def forward(self, x_func, x_loc):
-            
-        branch_outputs = torch.stack([branch(x_func) for branch in self.branches], dim=0)  # [dim_out, batch_size_branch, branch_dim]
-        trunk_outputs = torch.stack([trunk.activation(trunk(x_loc)) for trunk in self.trunks], dim=0)
+        
+        # Get one output for each branch (dim_out many)
+        branch_outputs = torch.stack([branch(x_func) for branch in self.branches], dim=0)  
+        # Shape (dim_out, #u, i): i = branch & trunk output dim
+        
+        # Get trunk ouput (only one)       
+        trunk_output = self.trunk.activation(self.trunk(x_loc))
+        # Shape (#t, i): i = branch & trunk output dim
+        
+        """ If needed in the future
+        # trunk_outputs = trunk_outputs.unsqueeze(0).expand(self.dim_out, *trunk_outputs.shape)        
+        # equivalent to torch.stack([self.trunk.activation(self.trunk(x_loc)) for _ in range(self.dim_out)], dim=0)"""
         
         # Equivalent to b @ t.T for each output, but in batch
-        out = torch.einsum("obi,oti->bto", branch_outputs, trunk_outputs) + self.bias # [batch_size, dim_out]
+        # Dot product trunk output with each branch output to get final output
+        output = torch.einsum("dui,ti->utd", branch_outputs, trunk_output) + self.bias 
+        # Shape (#u, #t, dim_out)
 
-        return out
+        return output
 
 
-
+# Compile and train model + data
 class Model():
     
-    def __init__(self, x_train, y_train, x_test, y_test, net, optimizer, loss_fn=nn.MSELoss()):     
+    def __init__(self, x_train, y_train, x_test, y_test, net, optimizer='adam', loss_fn=torch.nn.MSELoss(), lr=0.001):     
         
-        self.x_train = (self.format(x_train[0]), self.format(x_train[1]))
+        # Training data
+        self.x_func_train = self.format(x_train[0])
+        self.x_loc_train = self.format(x_train[1])
         self.y_train = self.format(y_train)
         
-        self.x_test = (self.format(x_test[0]), self.format(x_test[1]))
+        # Testing data
+        self.x_func_test = self.format(x_test[0])
+        self.x_loc_test = self.format(x_test[1])
         self.y_test = self.format(y_test)
         
+        # Network
         self.net = net
-        self.vlosshistory = []  # validation/test loss
-        self.tlosshistory = []  # training loss
         
-        self.optimizer = optimizer
+        # Loss history
+        self.tlosshistory = []  # training loss
+        self.vlosshistory = []  # validation/test loss
+
+        # Initialize optimizer (default adam)
+        optimizer = {'adam':torch.optim.Adam, 'sgd':torch.optim.SGD}[optimizer]
+        self.optimizer = optimizer(net.parameters(), lr=lr)
+        
+        # Set loss function (MSE default)
         self.loss_fn = loss_fn
         
+        
+    # Format data as torch tensor with dtype=float32    
     def format(self, x):
         x = x if isinstance(x, torch.Tensor) else torch.tensor(x)
         return x.to(torch.float32)
     
-                
-    def train_one_iteration(self):
+                        
         
-            self.optimizer.zero_grad()
-            prediction = self.net(self.x_train[0], self.x_train[1])   
-            loss = self.loss_fn(prediction, self.y_train)
-            loss.backward()
-            self.optimizer.step()
-            return loss.item()
-        
-        
+    # Train network
     def train(self, iterations):
         
+        # Train step history
         self.steps = []
         
+        # Set net to train mode
+        self.net.train(True)
+        
+        # Training process updates
         print('Step \t Train loss \t Test loss')
 
         for iter in range(iterations):
             
-            self.net.train(True)
-            tloss = self.train_one_iteration()
+            
+            # Train
+            self.optimizer.zero_grad()
+            prediction = self.net(self.x_func_train, self.x_loc_train)   
+            loss = self.loss_fn(prediction, self.y_train)
+            loss.backward()
+            self.optimizer.step()
+            tloss = loss.item()
 
+            # Test
             if iter % 1000 == 999:
-                
+                # Set net to evalutation mode
                 self.net.eval()
+                # Don't calculate gradients
                 with torch.no_grad():
-                    outputs = self.net(self.x_test[0], self.x_test[1])   
+                    outputs = self.net(self.x_func_test, self.x_loc_test)   
                     vloss = self.loss_fn(outputs, self.y_test).item()
-                
+                # Save loss history
                 self.vlosshistory.append(vloss)
                 self.tlosshistory.append(tloss)
                 self.steps.append(iter)
+                self.net.train(True)
                 
                 print('{} \t [{:.2e}] \t [{:.2e}]'.format(iter + 1, tloss, vloss))            
             
@@ -119,7 +154,8 @@ class Model():
         ax.grid(True)
         ax.legend()
         plt.show()
-            
+    
+    # Predict output using DeepONet
     def predict(self, x_func, x_loc):
         
         return self.net(self.format(x_func), self.format(x_loc))
