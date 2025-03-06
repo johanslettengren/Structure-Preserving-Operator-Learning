1adfdc23dae6a0a4283c6468712b019a2477b389
johansle <johansle@kth.se>
2025-03-06 15:44:34 +0000

    Intermediate save

--- Tests/save.py
+++ Tests/save.py
@@ -38,15 +38,16 @@ class FNN(torch.nn.Module):
         return z
 
 class branch(torch.nn.Module):
-    def __init__(self, layer_sizes, dim, K, activation):
+    def __init__(self, layer_sizes, K, N, activation, dim=2):
         super().__init__()
         
         # Set activation function
         self.activation = activation
-        self.K = K
         self.dim = dim
+        self.N = N
+        self.K = K
                 
-        layer_sizes = [2] + layer_sizes + [dim*K]
+        layer_sizes = [dim*N] + layer_sizes + [dim*K*N]
         # Create layers
         self.linears = torch.nn.ModuleList()
         for i in range(1, len(layer_sizes)):
@@ -59,11 +60,16 @@ class branch(torch.nn.Module):
         
     # Branch forward pass
     def forward(self, momenta):
-        z = momenta
+                
+        z = momenta.reshape(*momenta.shape[:-2], -1)
+        
         for linear in self.linears[:-1]:
             z = self.activation(linear(z))
             
-        z = self.linears[-1](z).view(-1, self.dim, self.K)
+       
+        z = self.linears[-1](z)
+        z = z.view(*z.shape[:-1], self.N, self.dim, self.K)
+        
              
 
         return z
@@ -99,30 +105,34 @@ class trunk(torch.nn.Module):
     
 # DeepONet class 
 class DeepONet(torch.nn.Module):
-    def __init__(self, layer_sizes_branch, layer_sizes_trunk, K=2, dim=2, activation='tanh', scheme=None, L=3, N=1):
+    def __init__(self, layer_sizes_branch, layer_sizes_trunk, K=2, activation='tanh', scheme=None, L=3, N=1):
         super(DeepONet, self).__init__()
         
         # Whether to QR factorise the branch
         self.scheme = scheme
+        
+        I = torch.eye(N)
+        O = torch.zeros_like(I)
+
+        self.J = torch.cat([torch.cat([O, I], dim=1), torch.cat([-I, O], dim=1) ], dim=0)
+        
+        #self.J = torch.tensor([[O, I], [-I, 0]], dtype=torch.float32)
 
         activation = {'relu' : torch.relu, 'tanh':torch.tanh, 'softplus' : torch.nn.Softplus(), 'htanh' : torch.nn.Hardtanh()}[activation]   
         
-        self.dim = dim
-            
             
         if scheme in ['preorth', 2]:
-            self.branch = FNN([2] + layer_sizes_branch + [2], activation)
+            self.branch = branch(layer_sizes_branch, K=1, N=N, activation=activation) 
             self.trunk = trunk(layer_sizes_trunk, K=K, activation=activation)        
 
         elif scheme in ['symplectic', 'symp', 3]:
-            self.branch = torch.nn.ModuleList([FNN([1] + layer_sizes_branch + [1], activation) for _ in range(L)]) 
+            self.branch = torch.nn.ModuleList([branch(layer_sizes_branch, 1, N, activation, dim=1) for _ in range(L)]) 
             self.trunk = trunk(layer_sizes_trunk, K=1, activation=activation)     
             self.a = torch.nn.ParameterList([0.01*torch.nn.Parameter(torch.randn(2, )) for _ in range(L)])
-            self.J = torch.tensor([[0, 1], [-1, 0]], dtype=torch.float32)
             self.L = L
 
         else:  
-            self.branch = branch(layer_sizes_branch, dim=dim, K=K, activation=activation) 
+            self.branch = branch(layer_sizes_branch, K=K, N=N, activation=activation) 
             self.trunk = trunk(layer_sizes_trunk, K=K, activation=activation)        
         
     
@@ -135,46 +145,57 @@ class DeepONet(torch.nn.Module):
         
         if self.scheme in ['QR', 1]:
             trunk_outputs  = self.trunk(t)
-            branch_outputs = self.branch(momenta)       
+            branch_outputs = self.branch(momenta)  
+            
             # orthogonalise branch outputs
             Q, R = torch.linalg.qr(branch_outputs)
+            
             # Rescale (so that norm matches energy) and redefine branch outputs
-            branch_outputs = Q*torch.linalg.norm(momenta, dim=1, keepdim=True).unsqueeze(-1)
+            branch_outputs = Q * torch.linalg.norm(momenta, dim=-1)[:,None,None]
             # Find the corresponding coordinates
-            trunk_outputs = torch.einsum("ukK,tK->utK", R, trunk_outputs)
+            
+
+            
+            trunk_outputs = torch.einsum("uNkK,tK->utNK", R, trunk_outputs)
+            
             # Normalise and redfine trunk outputs
-            trunk_outputs = torch.nn.functional.normalize(trunk_outputs, dim=2)
+            trunk_outputs = torch.nn.functional.normalize(trunk_outputs, dim=-1)
             # Get corresponding network output (trunk_outputs has one more dimension than usual)
-            output = torch.einsum("udK,utK->utd", branch_outputs, trunk_outputs)
+    
+            output = torch.einsum("uNdK,utNK->utNd", branch_outputs, trunk_outputs)
             
         elif self.scheme in ['preorth', 2]:
             trunk_outputs  = self.trunk(t)
             branch_outputs = self.branch(momenta)       
-            b1 = self.branch(momenta)
-            J = torch.tensor([[0, 1], [-1, 0]], dtype=torch.float32)
-            b2 = torch.einsum ('id, dk -> ik', b1, J)
-            branch_outputs = torch.stack((b1, b2), axis=-1)     
+            b1 = self.branch(momenta).squeeze(-1)
+            print(b1.shape)
+            print(self.J.shape)
+            b2 = torch.einsum ('uNd, dD -> uND', b1, self.J)          
+            print(b2.shape)  
+            branch_outputs = torch.stack((b1, b2), axis=-1)   
+            
             trunk_outputs = torch.nn.functional.normalize(trunk_outputs, dim=-1)
-            output = torch.einsum("udK,tK->utd", branch_outputs, trunk_outputs)   
+            output = torch.einsum("uNdK,tK->utNd", branch_outputs, trunk_outputs)   
             
         elif self.scheme in ['symplectic', 'symp', 3]:
             
-            momenta = momenta[:,None,:]
+            momenta = momenta[:,None,...]
+            
+            
             for i in range(self.L):
                 a = self.a[i]
-                alpha = self.trunk(t)[None, ...]
-                psi = self.branch[i](torch.einsum('utd, d -> ut', momenta, a)[...,None])
+                alpha = self.trunk(t)[None,:,:,None]   
+                psi = self.branch[i](torch.einsum('utNd, d -> utN', momenta, a)[...,None]).squeeze(-1)                
+                momenta = momenta + (alpha * (self.J @ a) * psi)
                 
-                momenta = momenta + alpha * (self.J @ a) * psi
+                
                 
             output = momenta
                                             
         else:
             trunk_outputs  = self.trunk(t)
-            branch_outputs = self.branch(momenta)   
-            
-            print(branch_outputs.shape)    
-            output = torch.einsum("udK,tK->utd", branch_outputs, trunk_outputs)     
+            branch_outputs = self.branch(momenta)
+            output = torch.einsum("uNdK,tK->utNd", branch_outputs, trunk_outputs)     
     
         return output
 
@@ -233,8 +254,8 @@ class Model():
             
             # Train
             self.optimizer.zero_grad()
-            outputs = self.net(*self.x_train)   
-            loss = self.loss_fn(outputs, self.y_train)
+            pred = self.net(*self.x_train)   
+            loss = self.loss_fn(pred, self.y_train)
 
 
             loss.backward()
@@ -250,8 +271,8 @@ class Model():
                 with torch.no_grad():
                     
                     
-                    outputs = self.net(*self.x_test)  
-                    vloss = self.loss_fn(outputs, self.y_test).item()
+                    pred = self.net(*self.x_test)  
+                    vloss = self.loss_fn(pred, self.y_test).item()
                     
                     announce_new_best = ''
                     if vloss < self.bestloss:
@@ -277,10 +298,8 @@ class Model():
         
         momenta = self.format(momenta, requires_grad=True)
         t = self.format(t, requires_grad=True)
-        
         u = self.net(momenta, t)
 
-        
         return u
     
     
@@ -296,114 +315,3 @@ class Model():
         ax.grid(True)
         ax.legend()
         plt.show()
-        
-    def plot_energy(self, q=1, p=1, Tmax=1, dpi=100):
- 
-        bv = np.array([[q, p]], dtype=np.float32)
-        t = np.linspace(0, Tmax, 20).reshape(-1, 1)
-        u = self.predict(bv, t)
-    
-         
-        E = torch.linalg.norm(u, axis=-1)[0,:]**2/2
-        _, ax = plt.subplots(figsize=(8, 2), dpi=dpi)
-        ax.set_title(f'Total Energy of DeepONet Prediction $(q_0, p_0)$=({q}, {p})')
-        ax.plot(t, np.ones_like(t)*(q**2 + p**2)/2, alpha=0.5, linewidth=5, label='True energy')
-        ax.plot(t, E.detach(), '--', alpha=0.8, linewidth=3,  label='DeepONet energy')
-        ax.legend()
-        ax.grid(True)
-        ax.set_xlabel("t")
-        ax.set_ylabel("Total energy")
-        plt.show()
-        
-    def plot_symplecticity(self, T=1, num_pts=100):
-
-                
-            grid_points = torch.linspace(-1, 1, num_pts, requires_grad=True, dtype=torch.float32)
-            mesh = torch.cartesian_prod(grid_points, grid_points)
-            t = torch.tensor([T], requires_grad=True, dtype=torch.float32).reshape(-1, 1)
-            u = self.net(mesh, t)[:,0,:]
-
-
-            D = torch.stack([grad(u[:,i], mesh, grad_outputs=torch.ones_like(u[:,i]), retain_graph=True)[0] for i in range(u.shape[1])], axis=-1)
-            J = torch.tensor([[0, 1], [-1, 0]], dtype=torch.float32)
-
-            DTJD = torch.matmul(D.transpose(1, 2), torch.matmul(J, D))
-            deltaJ = torch.linalg.norm(DTJD - J, ord=2, dim=(1, 2)).numpy().reshape(num_pts, num_pts)
-            
-            plt.figure(figsize=(8, 6))
-            plt.imshow(deltaJ, extent=(-1, 1, -1, 1), origin='lower', cmap='viridis')
-            plt.colorbar(label='$\Delta J$')
-            plt.title("$\Delta J$ over $(q,p)$-grid, for t=" + f"{T}")
-            plt.xlabel('q')
-            plt.ylabel('p')
-            plt.show()
-            
-    def plot_full_predictions(self, solver, Tmax, num_iv=3):
-        
-        iv = np.random.uniform(low=-1, high=1, size=(num_iv, 2)).astype(np.float32)
-        t = np.linspace(0, Tmax, 100)
-        pred  = self.predict(iv, t.reshape(-1, 1))
-        true = solver(iv, t.reshape(-1, 1))
-        
-        self.plot_predictions(pred, true, t, iv)
-        
-    def plot_rollout_predictions(self, solver, h=0.01, iters=1000, num_iv=3):
-
-
-        T = h * iters
-        h = torch.tensor([[h]], dtype=torch.float32)
-        
-        iv = np.random.uniform(low=-1, high=1, size=(num_iv, 2)).astype(np.float32)
-        conj_momenta = self.predict(self.format(iv), h)
-        
-        pred = conj_momenta.clone()
-        
-        for _ in range(iters):
-            conj_momenta = self.predict(conj_momenta.squeeze(1), h)
-            pred = torch.concatenate((pred, conj_momenta), axis=1)
-            
-        t = np.linspace(0, T, iters+1)
-        true = solver(iv, t.reshape(-1, 1))
-                        
-        self.plot_predictions(pred, true, t, iv, title='Rollout prediction')
-
-    
-    def plot_predictions(self, pred, true, t, iv, title='Prediction'):
-
-
-        # Plot prediction vs. solution
-        fig, axes = plt.subplots(2, 2, figsize=(20, 4), dpi=150)
-
-        axes[0][0].set_title(title)
-        axes[0][1].set_title('Error')
-        
-        handles, labels = [], []
-        
-        for k in range(axes.shape[0]):
-            for l in range(pred.shape[0]):
-                
-                label = '({:.1f}, {:.1f})'.format(iv[l, 0], iv[l, 1])
-                axes[k][0].plot(t, true[l, :, k], label=label, alpha=0.5, linewidth=5)
-                axes[k][0].plot(t, pred[l, :, k].detach(), '--', c='k')
-                line, = axes[k][1].plot(t, true[l, :, k] - pred[l, :, k].detach(), label=label, linewidth=2)
-
-                # Store handles and labels only once
-                if k == 0:
-                    handles.append(line)
-                    labels.append(label)
-
-            axes[k][0].grid(True)
-            axes[k][1].grid(True)
-
-        axes[k][0].set_xlabel("t")
-        axes[k][1].set_xlabel("t")
-
-        # Create a shared legend
-        fig.legend(handles, labels, loc="upper center", ncol=len(labels), fontsize=10)
-
-        plt.show()
-        
-        
-        
-                
-        
